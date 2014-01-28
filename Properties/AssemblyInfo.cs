f4e4bc4f5661d529e58f147e7bf5d51e7125e463
TheBerkin <grenzatuna@gmail.com>
2014-01-27 22:17:32 -0600

    Updated version info/metadata

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PINE Framework")]
+[assembly: AssemblyTitle("Procedural Iterative Numerical Effects Framework")]
 [assembly: AssemblyDescription("Smells like pine. Feels like pine. Tastes like pine.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Moist Bat Industries")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PINE Framework")]
-[assembly: AssemblyCopyright("Copyright © Moist Bat Industries 2013")]
+[assembly: AssemblyCopyright("Copyright © Nicholas Fleck 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
